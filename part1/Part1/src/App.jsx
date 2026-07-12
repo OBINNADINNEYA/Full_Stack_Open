61dceb0005e05c4f9d552b85a53634dd57554405
obinna dinneya <obinnadinneya@yahoo.com>
2026-07-12 14:43:06 -0400

    exercise 1.1 and 1.2

--- part1/Part1/src/App.jsx
+++ part1/Part1/src/App.jsx
@@ -5,9 +5,19 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
+const Part = (props) => {
+  return (
+    <p> {props.part} {props.exercises} </p>
+  )
+}
+
 const Content = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <div>
+      <Part part={props.p1} exercises={props.e1} />
+      <Part part={props.p2} exercises={props.e2} />
+      <Part part={props.p3} exercises={props.e3} />
+    </div>
   )
 }
 
@@ -29,9 +39,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content p1={part1} p2={part2} p3={part3} e1={exercises1} e2={exercises2} e3={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
